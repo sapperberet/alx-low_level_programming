4ba987743d7f8ab72b34edeebbccc25695284d04
root <root@DESKTOP-PQCASE0.localdomain>
2023-02-16 18:48:15 +0200

    modami

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	    puts("\"Programming is like building a multilingual puzzle");
+	        return 0;
+
+
+		//"Programming is like building a multilingual puzzle
+		////use puts
+		////no printf
+		////return0
+
+}
