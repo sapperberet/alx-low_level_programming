3f26c61cc7a854f9769de1190c6d1b75e28cd609
root <root@DESKTOP-PQCASE0.localdomain>
2023-02-24 06:47:25 +0200

    fs

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - entry point
+ * @n: int
+ */
+void print_number(int n)
+{
+unsigned int n1;
+
+n1 = n
+
+if (n < 0)
+{
+	n1 = -n;
+
+	_putchar('-');
+}
+
+if (n1 / 10)
+{
+	print_number(n1 / 10);
+}
+
+_putchar((n1 % 10) + '0');
+}
