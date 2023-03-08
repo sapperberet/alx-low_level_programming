cb116bbf88d4336a1f8f2ba6074c71734c710c9f
root <root@DESKTOP-PQCASE0.localdomain>
2023-03-08 21:31:15 +0200

    nmistake

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,7 @@ void _puts_recursion(char *s)
 if (*s == '\0')
 {
 	_putchar('\n');
-	return (0);
+	return;
 }
 _putchar(*s);
 _puts_recursion(s + 1);
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,14 @@
 #include "main.h"
 
 /**
- * *_memcpy - entry point
- * @dest: string 1
- * @src:  string 2
- * @n: int
- * Return: pointer
+ *_print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void _print_rev_recursion(char *s)
 {
-unsigned int i;
-
-for (i = 0; i < n; i++)
+if (*s)
 {
-	dest[i] = src[i];
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
-
-return (dest);
 }
