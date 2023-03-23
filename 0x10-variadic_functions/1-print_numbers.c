5bcc1d937e4ecfb4a80cb9545da121f5fd08a621
root <root@DESKTOP-PQCASE0.localdomain>
2023-03-23 23:14:39 +0200

    first

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - entry point
+ * @separator: str
+ * @n: num
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;x
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
+	}
+
+	va_end(list);
+
+	printf("\n");
+}
