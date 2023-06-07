cb4f26eeb5be25c0552c4b9991a36d9d1f590d2e
sapperberet <93820325+sapperberet@users.noreply.github.com>
2023-06-07 15:26:39 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,12 +4,11 @@
  * _puts_recursion - entry point
  *@s: str
  */
-void _puts_recursion(char *s)
+ _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
 	_putchar('\n');
-	return;
 }
 _putchar(*s);
 _puts_recursion(s + 1);
